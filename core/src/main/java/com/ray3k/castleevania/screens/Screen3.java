b4bff0275bea5c585185e31b57c3758cf602c3f3
raeleus <raymond.ray3k@gmail.com>
2025-12-14 20:41:32 -0800

    Finished the game.

--- core/src/main/java/com/ray3k/castleevania/screens/Screen3.java
+++ core/src/main/java/com/ray3k/castleevania/screens/Screen3.java
@@ -4,28 +4,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.esotericsoftware.spine.AnimationState;
 import com.esotericsoftware.spine.AnimationState.AnimationStateAdapter;
 import com.esotericsoftware.spine.AnimationState.TrackEntry;
 import com.esotericsoftware.spine.AnimationStateData;
+import com.esotericsoftware.spine.Event;
 import com.github.tommyettinger.textra.FWSkin;
+import com.github.tommyettinger.textra.TypingLabel;
 import com.ray3k.castleevania.SpineImage;
 
 import static com.ray3k.castleevania.Main.*;
 
-public class Screen2 extends ScreenAdapter {
+public class Screen3 extends ScreenAdapter {
     private Stage stage;
     private FWSkin skin;
     private FitViewport viewport;
@@ -33,11 +34,12 @@ public class Screen2 extends ScreenAdapter {
     private SpineImage backgroundImage;
     private int mode = 0;
     private float amount;
-    private Music crankSound;
+    private Music slapSound;
+    private TypingLabel label;
 
     @Override
     public void show() {
-        crankSound = Gdx.audio.newMusic(Gdx.files.internal("sfx/drawbridge.ogg"));
+        slapSound = Gdx.audio.newMusic(Gdx.files.internal("sfx/slap.ogg"));
 
         viewport = new FitViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
         stage = new Stage(viewport, batch);
@@ -53,7 +55,7 @@ public class Screen2 extends ScreenAdapter {
         var stack = new Stack();
         root.add(stack).grow();
 
-        var skeletonData = skeletonJson.readSkeletonData(Gdx.files.internal("scene2.json"));
+        var skeletonData = skeletonJson.readSkeletonData(Gdx.files.internal("scene3.json"));
         var animationData = new AnimationStateData(skeletonData);
         backgroundImage = new SpineImage(skeletonRenderer, skeletonData, animationData);
         backgroundImage.setCrop(0, 0, 1920, 1080);
@@ -63,8 +65,32 @@ public class Screen2 extends ScreenAdapter {
         var skeleton = backgroundImage.getSkeleton();
         var animation = backgroundImage.getAnimationState();
 
+        var container = new Container<>();
+        stack.add(container);
+
+        label = new TypingLabel("", skin);
+        label.setWrap(true);
+        container.setActor(label);
+        container.padLeft(439);
+        container.padRight(500);
+        container.padTop(63);
+        container.padBottom(594);
+        container.top().left();
+        container.fill();
+
         animation.setAnimation(0, "hold", true);
         animation.setAnimation(1, "fade-in", false);
+        animation.addListener(new AnimationStateAdapter() {
+            @Override
+            public void event(TrackEntry entry, Event event) {
+                if (event.getData().getAudioPath() != null) {
+                    var music = Gdx.audio.newMusic(Gdx.files.internal(event.getData().getAudioPath()));
+                    music.play();
+                } else if (event.getString() != null) {
+                    label.restart(event.getString().replace("\\n", "\n"));
+                }
+            }
+        });
 
         skeletonData = skeletonJson.readSkeletonData(Gdx.files.internal("dmitrii.json"));
         animationData = new AnimationStateData(skeletonData);
@@ -72,14 +98,14 @@ public class Screen2 extends ScreenAdapter {
         playerImage.spineDrawable.drawWithoutScale = true;
         playerImage.clip = false;
         stage.addActor(playerImage);
-        playerImage.setY(250);
+        playerImage.setPosition(1890, 117);
 
         skeleton = playerImage.getSkeleton();
         skeleton.setScale(.5f, .5f);
         animation = playerImage.getAnimationState();
         animation.getData().setDefaultMix(.5f);
 
-        animation.setAnimation(0, "stand", true);
+        animation.setAnimation(0, "walk", true);
 
         stage.addListener(new InputListener() {
             @Override
@@ -90,8 +116,6 @@ public class Screen2 extends ScreenAdapter {
         });
     }
 
-    private static Vector2 temp = new Vector2();
-
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.BLACK);
@@ -100,69 +124,53 @@ public class Screen2 extends ScreenAdapter {
         var animation = playerImage.getAnimationState();
 
         if (mode == 0) {
-            if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("walk")) animation.setAnimation(0, "walk", true);
-                skeleton.setScaleX(-.5f);
-                playerImage.setX(playerImage.getX() + 100 * delta);
-                if (playerImage.getX() >= 700) {
-                    playerImage.setX(700);
-                    playerImage.getAnimationState().setAnimation(0, "crank", true);
-                    mode++;
-                    backgroundImage.getAnimationState().setAnimation(0, "bridge", false);
-                    backgroundImage.getAnimationState().getCurrent(0).setTimeScale(0);
-                    backgroundImage.getAnimationState().setAnimation(1, "space", true);
-                }
-            } else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("walk")) animation.setAnimation(0, "walk", true);
-                skeleton.setScaleX(.5f);
-                playerImage.setX(playerImage.getX() - 100 * delta);
-            } else {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("stand")) animation.setAnimation(0, "stand", true);
+            playerImage.setX(playerImage.getX() - 100 * delta);
+            if (playerImage.getX() < 1467) {
+                mode++;
+                playerImage.setX(1467);
+                playerImage.getAnimationState().setAnimation(0, "stand", true);
+
+                backgroundImage.getAnimationState().setAnimation(0, "speech", false);
+                backgroundImage.getAnimationState().addListener(new AnimationStateAdapter() {
+                    @Override
+                    public void complete(TrackEntry entry) {
+                        if (mode == 1) {
+                            mode++;
+                            backgroundImage.getAnimationState().setAnimation(0, "fight-start", false);
+                            backgroundImage.getAnimationState().addAnimation(0, "fight-loop", true, 0);
+
+                            playerImage.remove();
+                            label.setVisible(false);
+                        }
+                    }
+                });
+                bgm.setVolume(.2f);
             }
         } else if (mode == 1) {
+            //do nothing
+        } else if (mode == 2) {
             if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
                 amount += .05f;
-                crankSound.play();
-                crankSound.setPosition(0);
+                slapSound.play();
+                slapSound.setPosition(0);
             }
             amount -= .01f * delta;
             if (amount < 0) amount = 0;
-            backgroundImage.getAnimationState().getCurrent(0).setTrackTime(amount);
             if (amount >= 1) {
                 amount = 1;
                 mode++;
-                playerImage.getAnimationState().setAnimation(0, "stand", true);
-                backgroundImage.getAnimationState().setAnimation(1, "hold", false);
-            }
-        } else if (mode == 2) {
-            if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("walk")) animation.setAnimation(0, "walk", true);
-                skeleton.setScaleX(-.5f);
-                playerImage.setX(playerImage.getX() + 100 * delta);
-                if (playerImage.getX() >= 1800) {
-                    playerImage.setX(1800);
-                    playerImage.getAnimationState().setAnimation(0, "fade-out", false);
-                    mode++;
-                    backgroundImage.getAnimationState().setAnimation(1, "fade-out", false);
-                    backgroundImage.getAnimationState().addListener(new AnimationStateAdapter() {
-                        @Override
-                        public void complete(TrackEntry entry) {
-                            nextScreen();
-                        }
-                    });
-                }
-            } else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("walk")) animation.setAnimation(0, "walk", true);
-                skeleton.setScaleX(.5f);
-                playerImage.setX(playerImage.getX() - 100 * delta);
-            } else {
-                var anim = animation.getCurrent(0);
-                if (!anim.getAnimation().getName().equals("stand")) animation.setAnimation(0, "stand", true);
+                backgroundImage.getAnimationState().setAnimation(0, "fight-death", false);
+                backgroundImage.getAnimationState().addAnimation(0, "skeleton", false, 2);
+                slapSound.stop();
+                label.setVisible(true);
+                label.setText("");
+
+                backgroundImage.getAnimationState().addListener(new AnimationStateAdapter() {
+                    @Override
+                    public void complete(TrackEntry entry) {
+                        if (entry.getAnimation().getName().equals("skeleton")) nextScreen();
+                    }
+                });
             }
         }
 
@@ -176,6 +184,6 @@ public class Screen2 extends ScreenAdapter {
     }
 
     private void nextScreen() {
-        main.setScreen(new Screen3());
+        main.setScreen(new Screen1());
     }
 }
